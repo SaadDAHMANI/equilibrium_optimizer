f350dc2023953a9e726d6d499044478969c24f1b
SaadDAHMANI <tahatatakor7@gmail.com>
2021-09-03 16:13:49 +0100

    print results
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ include!("benchmarks.rs");
 fn main() {
     println!("Equilibrium Optimizer (EO)");
 
-    let particuls = 10;
-    let kmax =1;
+    let particuls = 30;
+    let kmax =500;
     let lb =-100.00;
     let ub = 100.00;
-    let dim = 3;
+    let dim = 12;
 
-    run_eo(particuls, kmax, lb,ub, dim, &f1);
-    
+   let (fbest, bestsol, convergcrv) = run_eo(particuls, kmax, lb,ub, dim, &f1);
+
+    println!("best fitness : {}", fbest);
+    println!("best solution : {:?}", bestsol);
+    println!("best fitness CV {:?}", convergcrv[convergcrv.len()-1]);
+
 }
