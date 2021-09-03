fb153741ffb552afe093f31a75d5efa2ce5219fa
SaadDAHMANI <tahatatakor7@gmail.com>
2021-09-03 11:39:53 +0100

    add initialization of search agents.
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- src/equiibrium_optimizer.rs
+++ src/equiibrium_optimizer.rs
@@ -1,5 +1,24 @@
 
-pub fn run_eo()->f64 {
-    let result = 0.0;
-    result
+use rand::distributions::Uniform;
+use rand::distributions::Distribution;
+
+pub fn run_eo() {
+    
+
+
+    
+}
+
+fn initialization(searchagents_no : usize, dim : usize, lb : f64, ub : f64)-> Vec<Vec<f64>>{
+    let mut positions = vec![vec![0.0f64; dim]; searchagents_no];
+    let intervall01 = Uniform::from(0.0f64..=1.0f64);
+    let mut rng = rand::thread_rng();              
+    
+    for i in 0..searchagents_no {
+         for  j in 0..dim {   
+              positions[i][j]= intervall01.sample(&mut rng)*(ub-lb)+lb;                         
+         }
+    }    
+    
+    positions
 }
