3a16e99ec172627fc37a67fa8f80f887bf0cfddb
SaadDAHMANI <tahatatakor7@gmail.com>
2021-09-03 16:24:17 +0100

    add time measurment
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- src/benchmarks.rs
+++ src/benchmarks.rs
@@ -8,3 +8,18 @@ pub fn f1(x : &Vec<f64>)-> f64 {
     }  
     sum
 }
+
+fn f2(x: &Vec<f64>)-> f64 {
+    //o=sum(abs(x))+prod(abs(x));
+    let mut sum : f64 = 0.0;
+    let mut prod : f64 =1.0;
+
+     for value in x.iter(){
+         sum += value.abs();
+         prod *=value.abs();
+     }  
+       sum + prod    
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,27 @@
 include!("equiibrium_optimizer.rs");
 include!("benchmarks.rs");
 
-fn main() {
-    println!("Equilibrium Optimizer (EO)");
+use std::time::Instant;
 
+fn main() {
+    println!("Equilibrium Optimizer (EO) is run ..... ");
+    
     let particuls = 30;
-    let kmax =500;
+    let kmax = 500;
     let lb =-100.00;
     let ub = 100.00;
-    let dim = 12;
+    let dim = 30;
 
-   let (fbest, bestsol, convergcrv) = run_eo(particuls, kmax, lb,ub, dim, &f1);
+    let chronos = Instant::now();
+
+    let (fbest, bestsol, convergcrv) = run_eo(particuls, kmax, lb,ub, dim, &f2);
+
+    let duration = chronos.elapsed();
 
     println!("best fitness : {}", fbest);
-    println!("best solution : {:?}", bestsol);
+    //println!("best solution : {:?}", bestsol);
     println!("best fitness CV {:?}", convergcrv[convergcrv.len()-1]);
+
+    println!("End computation in : {:?}", duration);
 
 }
